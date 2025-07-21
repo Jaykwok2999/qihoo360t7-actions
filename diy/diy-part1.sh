15e9719b5fed278a470cbbbc02ecdfe26b3215da
Jaykwok2999 <61024435@qq.com>
2025-07-22 00:32:00 +0800

    Update diy-part1.sh

--- diy/diy-part1.sh
+++ diy/diy-part1.sh
@@ -14,9 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git third_party https://github.com/linkease/istore-packages.git;main' >>feeds.conf.default
 echo 'src-git istoreos_ipk https://github.com/Jaykwok2999/istoreos-ipk.git;main' >>feeds.conf.default  
 echo 'src-git theme https://github.com/Jaykwok2999/openwrt-theme.git;main' >>feeds.conf.default
 echo 'src-git turboacc https://github.com/chenmozhijin/turboacc.git;luci' >>feeds.conf.default
