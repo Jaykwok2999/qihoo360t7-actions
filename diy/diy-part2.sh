9ef24b4290bddd84cbca2966be6e2afa2a89b2b0
Jaykwok2999 <61024435@qq.com>
2025-07-22 18:49:02 +0800

    Update diy-part2.sh

--- diy/diy-part2.sh
+++ diy/diy-part2.sh
@@ -73,7 +73,7 @@ function merge_package() {
 }
 
 
-git_sparse_clone main https://github.com/Jaykwok2999/openwrt-theme luci-app-argon-config
+#git_sparse_clone main https://github.com/Jaykwok2999/openwrt-theme luci-app-argon-config
 #git_sparse_clone main https://github.com/Jaykwok2999/istoreos-ota luci-app-ota
 #git_sparse_clone main https://github.com/zijieKwok/github-ota fw_download_tool
 
