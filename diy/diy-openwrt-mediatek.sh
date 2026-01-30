3e3e51847d5f9e296f45033009bb103b70e56e98
Jaykwok2999 <61024435@qq.com>
2026-01-30 17:23:15 +0800

    Update diy-openwrt-mediatek.sh

--- diy/diy-openwrt-mediatek.sh
+++ diy/diy-openwrt-mediatek.sh
@@ -55,7 +55,6 @@ rm -rf feeds/istoreos_ipk/linkease
 rm -rf feeds/istoreos_ipk/tailscale
 # 移除 openwrt feeds 自带的核心库
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 
 # istoreos-theme
 rm -rf feeds/luci/themes/luci-theme-argon
