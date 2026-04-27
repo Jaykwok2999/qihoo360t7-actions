ba81f634147af1189d293df59de81bf921bbabcb
Jaykwok2999 <61024435@qq.com>
2026-04-27 12:18:14 +0800

    Update diy-openwrt-mediatek.sh

--- diy/diy-openwrt-mediatek.sh
+++ diy/diy-openwrt-mediatek.sh
@@ -150,28 +150,6 @@ rm -rf package/base-files/files/etc/banner
 # cp -af feeds/istoreos_ipk/patch/OpenWrt/SNAPSHOT/banner package/base-files/files/etc/
 cp -af feeds/istoreos_ipk/patch/banner package/base-files/files/etc/
 
-PKG_PATH="$GITHUB_WORKSPACE/openwrt/package/"
-
-#修复TailScale配置文件冲突
-TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
-if [ -f "$TS_FILE" ]; then
-	echo " "
-
-	sed -i '/\/files/d' $TS_FILE
-
-	cd $PKG_PATH && echo "tailscale has been fixed!"
-fi
-
-#修复Rust编译失败
-RUST_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
-if [ -f "$RUST_FILE" ]; then
-	echo " "
-
-	sed -i 's/ci-llvm=true/ci-llvm=false/g' $RUST_FILE
-
-	cd $PKG_PATH && echo "rust has been fixed!"
-fi
-
 
 
 ./scripts/feeds update -a
